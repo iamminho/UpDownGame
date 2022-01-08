1e79671857fc9c853deec756f8938b9a4ad8329d
softpaper95 <minho100227@gamil.com>
2022-01-08 22:42:47 +0900

    complete UI v.1

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 이민호 on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
+}
+
